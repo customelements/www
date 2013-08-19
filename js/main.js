40082ebbc052c2778ec1859765b61a7256fa6a96
Zeno Rocha <zno.rocha@gmail.com>
2013-08-19 15:00:13 -0300

    Filter by most popular, add submit your own and remove date column

--- js/main.js
+++ js/main.js
@@ -3,43 +3,39 @@
 	'use strict';
 
 	$(function () {
-		$.getJSON('http://grunt-plugin-list.herokuapp.com', function (modules) {
-			// only show plugins created after the specified date
-			modules = _.filter(modules, function (el) {
-				return Date.parse(el.time.created) > new Date('1800-01-01');
-			});
+		$.getJSON('data/repos.json', function (modules) {
 
-			var latestModules = _.sortBy(modules, function (el) {
-				return -Date.parse(el.time.created);
-			}).splice(0, 5);
+			var topModules = _.sortBy(modules, function (el) {
+				return el.stars;
+			}).reverse().splice(0, 3);
+
+			console.log(topModules);
 
 			var allModules = _.sortBy(modules, function (el) {
 				// removing `grunt-` since some plugins don't contain it
 				return el.name.replace('grunt-', '');
 			});
 
-			var latestTpl = _.template($('#plugins-latest-template').html(), {
-				modules: latestModules
+			var latestTpl = _.template($('#most-popular-template').html(), {
+				modules: topModules
 			});
 
-			var allTpl = _.template($('#plugins-all-template').html(), {
+			var allTpl = _.template($('#all-template').html(), {
 				modules: allModules
 			});
 
 			$('#loading').remove();
-			$('#plugins-latest').append(latestTpl);
-			$('#plugins-all').append(allTpl).find('.search').show();
+			$('#most-popular').append(latestTpl);
+			$('#all').append(allTpl).find('.search').show();
 
-			new List('plugins-all', {
+			new List('all', {
 				valueNames: [
 					'name',
 					'desc',
 					'author',
-					'modified'
+					'stars'
 				]
 			});
-
-			$('#plugins-all .modified time').timeago();
 		});
 	});
 })(window, jQuery);
