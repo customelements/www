d6df864ef2f15f49f309098e648a429e13ad7b2e
Zeno Rocha <zno.rocha@gmail.com>
2013-08-20 16:57:14 -0300

    Use HTML5 validation instead of JS

--- js/github.js
+++ js/github.js
@@ -7,26 +7,16 @@
 		instance.handleFormSubmit();
 	};
 
-	GithubIssue.prototype.isValidRepo = function (repo) {
-		var regex = /^(http(s)?:\/\/)?github.com\/[A-Za-z0-9\.]+\/[A-Za-z0-9\.]+$/;
-
-		return repo.match(regex);
-	};
-
 	GithubIssue.prototype.handleFormSubmit = function () {
 		var instance = this;
 
 		$('#form-github-add').on('submit', function (e) {
 			e.preventDefault();
 
-			var repositoryURL = $('#repo').val();
+			var repoURL = $('#repo').val();
+			var repoName = repoURL.substring(repoURL.lastIndexOf('/') + 1, repoURL.length);
 
-			if (instance.isValidRepo(repositoryURL)) {
-				win.location.href = 'https://github.com/customelements/customelements.io/issues/new?title=Adding some new component for ' + repositoryURL + '&body=' + repositoryURL;
-			}
-			else {
-				alert('Please, type a valid repository!');
-			}
+			win.location.href = 'https://github.com/customelements/customelements.io/issues/new?title=Add new component - ' + repoName + '&body=' + repoURL;
 		});
 	};
 
