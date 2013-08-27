b9865adce81050ca6e7a68d5f921d7210d55e353
Zeno Rocha <zno.rocha@gmail.com>
2013-08-26 23:51:45 -0300

    Using fetch-service instead of local json

--- js/main.js
+++ js/main.js
@@ -3,40 +3,37 @@
 	'use strict';
 
 	$(function () {
-		$.getJSON('data/repos.json', function (modules) {
+		var modules = customElements;
 
-			var topModules = _.sortBy(modules, function (el) {
-				return el.stars;
-			}).reverse().splice(0, 3);
+		var topModules = _.sortBy(modules, function (el) {
+			return el.stars;
+		}).reverse().splice(0, 3);
 
-			console.log(topModules);
+		var allModules = _.sortBy(modules, function (el) {
+			el.name = el.name.replace('-element', '');
+			return el.name;
+		});
 
-			var allModules = _.sortBy(modules, function (el) {
-				// removing `grunt-` since some plugins don't contain it
-				return el.name.replace('grunt-', '');
-			});
+		var latestTpl = _.template($('#most-popular-template').html(), {
+			modules: topModules
+		});
 
-			var latestTpl = _.template($('#most-popular-template').html(), {
-				modules: topModules
-			});
+		var allTpl = _.template($('#all-template').html(), {
+			modules: allModules
+		});
 
-			var allTpl = _.template($('#all-template').html(), {
-				modules: allModules
-			});
+		$('#loading').remove();
+		$('#most-popular').append(latestTpl);
+		$('#all').append(allTpl).find('.search').show();
 
-			$('#loading').remove();
-			$('#most-popular').append(latestTpl);
-			$('#all').append(allTpl).find('.search').show();
-
-			new List('all', {
-				valueNames: [
-					'name',
-					'desc',
-					'stars',
-					'forks',
-					'author'
-				]
-			});
+		new List('all', {
+			valueNames: [
+				'name',
+				'desc',
+				'stars',
+				'forks',
+				'author'
+			]
 		});
 	});
 })(window, jQuery);
