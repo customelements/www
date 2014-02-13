42b37ebc5f1d068e18131f6fa093d968a6e4b4fa
Luan Muniz <luan@luanmuniz.com.br>
2014-02-13 20:46:31 -0200

    Add Latest elements section content

--- js/main.js
+++ js/main.js
@@ -9,6 +9,10 @@
             return el.stars;
         }).reverse().splice(0, 3);
 
+        var lastModules = _.sortBy(modules, function (el) {
+            return el.created;
+        }).reverse().splice(0, 3);
+
         var allModules = _.sortBy(modules, function (el) {
             el.name = el.name.replace('-custom-element', '');
             el.name = el.name.replace('-element', '');
@@ -16,8 +20,12 @@
             return el.name;
         });
 
-        var latestTpl = _.template($('#most-popular-template').html(), {
+        var mostPopTpl = _.template($('#most-popular-template').html(), {
             modules: topModules
+        });
+
+        var latestTpl = _.template($('#latest-elements-template').html(), {
+            modules: lastModules
         });
 
         var allTpl = _.template($('#all-template').html(), {
@@ -25,7 +33,8 @@
         });
 
         $('#loading').remove();
-        $('#most-popular').append(latestTpl);
+        $('#most-popular').append(mostPopTpl);
+        $('#latest-popular').append(latestTpl);
         $('#all').append(allTpl).find('.search').show();
 
         new List('all', {
