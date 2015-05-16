4eecd87d867b8d93c9521c0d295a20499c7842ff
Zeno Rocha <hi@zenorocha.com>
2015-05-16 16:37:58 -0700

    Moves public files to assets folder

--- routes.js
+++ routes.js
@@ -6,12 +6,9 @@ module.exports = [
     },
     {
         method: 'GET',
-        path: '/{param*}',
+        path: '/assets/{param*}',
         handler: {
-            directory: {
-                path: 'public',
-                listing: true
-            }
+            directory: { path: 'assets' }
         }
     }
 ];
