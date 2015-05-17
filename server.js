51dd4965bbef15da65442362deb8cb2561fcdeb7
Zeno Rocha <hi@zenorocha.com>
2015-05-17 09:07:06 -0700

    Strips trailing slash from routes

--- server.js
+++ server.js
@@ -7,7 +7,10 @@ var Hapi = require('hapi');
 var server = new Hapi.Server();
 
 server.connection({
-    port: process.env.PORT || 3000
+    port: process.env.PORT || 3000,
+    router: {
+        stripTrailingSlash: true
+    }
 });
 
 server.route(require('./routes'));
