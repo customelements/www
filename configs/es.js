0d955b7c12f3e05c56380e02f33b2ee953686118
Zeno Rocha <hi@zenorocha.com>
2016-01-07 22:21:39 -0800

    Updates footer link color and removes unused file

--- configs/es.js
+++
@@ -1,9 +0,0 @@
-var elasticsearch = require('elasticsearch');
-
-module.exports = function() {
-    var client = new elasticsearch.Client({
-        host: process.env.FOUNDELASTICSEARCH_URL
-    });
-
-    return client;
-}();
